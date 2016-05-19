0b90a2b2e6f8db711331ef8631e9029a61082ebc
Valentine Paramonov <valentins.paramonovs@4finance.com>
2016-05-19 10:58:49 +0300

    Make MapLoader public

--- src/test/groovy/paramonov/valentine/taf/MapLoader.groovy
+++ src/test/groovy/paramonov/valentine/taf/MapLoader.groovy
@@ -1,11 +1,9 @@
 package paramonov.valentine.taf
 
 import freemind.Map
-import groovy.transform.PackageScope
 
 import javax.xml.bind.JAXBContext
 
-@PackageScope
 class MapLoader {
     static Map loadMapFrom(String resourcePath) {
         def map = MapLoader.classLoader.getResource(resourcePath)
